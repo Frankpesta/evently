f80072790843a80de5446fb6553357795ca62965
Frankpesta <franklinolisaemeka3@gmail.com>
2024-09-13 12:19:44 +0100

    Fixed potential issues

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isPublicRoute = createRouteMatcher([
-	"/sign-in(.*)",
-	"/sign-up(.*)",
-	"/",
-	"/events/:id",
-	"/api/webhook/clerk",
-	"/api/webhook/stripe",
-	"/api/uploadthing",
-]);
-
-const isProtectedRoute = createRouteMatcher(["/(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-	if (!isPublicRoute(req)) {
-		auth().protect();
-	}
+export default authMiddleware({
+	publicRoutes: [
+		"/",
+		"/events/:id",
+		"/api/webhook/clerk",
+		"/api/webhook/stripe",
+		"/api/uploadthing",
+	],
+	ignoredRoutes: [
+		"/api/webhook/clerk",
+		"/api/webhook/stripe",
+		"/api/uploadthing",
+	],
 });
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)",
-	],
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
